3324933cb070091922d5ddb10bee221005cc5a9d
allen-mo <735016387@qq.com>
2017-03-01 18:02:48 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,86 @@
+<style src="todomvc-app-css/index.css">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input type="text"
+        class="new-todo"
+        autofocus
+        placeholder="what nedds to be done?"
+        @keyup.enter="addTodo">
+    </header>
+    <section class="main" v-if="todos.length">
+      <input type="chexkbox"
+        class="toggle-all"
+        :checked="allChecked"
+        @change="toggleAll({ done: !allChecked })">
+        <ul class="todo-list">
+          <Todo v-for="(todo, index) in filteredTodos" :key="index" :todo="todo"></Todo>
+        </ul>
+    </section>
+  </section>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
-</script>
+  import { mapMutations } from 'vuex'
+  import Todo from './components/todo.vue'
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  const filters = {
+    all: todos => todos,
+    active: todos => todos.filter(todo => !todo.done),
+    completed: todos => todos.filter(todo => todo.done)
+  }
+
+  export default {
+    components: { Todo },
+    data () {
+      return {
+        visibility: 'all',
+        filters: filters
+      }
+    },
+
+    computed: {
+      todos () {
+        return this.$store.state.todos
+      },
+      allChecked () {
+        return this.todos.every(todo => todo.done)
+      },
+      filteredTodos () {
+        return filters[this.visibility](this.todos)
+      },
+      remaining () {
+        return this.todos.filter(todo => !todo.done).length
+      }
+    },
+
+    methods: {
+      addTodo (e) {
+        let text = e.target.value
+        if (text.trim()) {
+          this.$store.commit('addTodo', { text })
+        }
+        e.target.value = ''
+      },
+      ...mapMutations([
+        'toggleAll',
+        'clearCompleted'
+      ])
+    },
+
+    filters: {
+      pluralize: (n, w) => n === 1 ? w : (w + 's'),
+      capitalize: s => s.charAt(0).toUpperCase() + s.slice(1)
+    }
+  }
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const TODO_LIST_STORAGE_KEY = 'todo-list'
+export default new Vuex.Store({
+  state: {
+    todos: JSON.parse(window.localStorage.getItem(TODO_LIST_STORAGE_KEY) || '[]')
+  },
+
+  mutations: {
+    addTodo: (state, { text }) => {
+      console.log(text)
+      state.todos.push({
+        text,
+        done: false
+      })
+    },
+
+    deleteTodo: (state, { todo }) => {
+      state.todos.splice(state.todos.indexOf(todo), 1)
+    },
+
+    toggleTodo: (state, { todo }) => {
+      todo.done = !todo.done
+    },
+
+    editTodo: (state, { todo, value }) => {
+      console.log('here')
+      todo.text = value
+    }
+  }
+})
